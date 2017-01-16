2cb50c3921de386c39b44344cd0d6856fc4820fb
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-16 21:08:05 +0200

    L13. Blade

--- resources/views/default/layouts/layout.blade.php
+++ resources/views/default/layouts/layout.blade.php
@@ -20,7 +20,7 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li class="active"><a href="{{ route('home'); }}">Home</a></li>
+                <li class="active"><a href="{{ route('home') }}">Home</a></li>
                 <li><a href="{{ route('about') }}">About</a></li>
                 <li><a href="{{route('articles') }}">Articles</a></li>
                 <li><a href="{{ route('article',array('id'=>10)) }}">Article</a></li>
