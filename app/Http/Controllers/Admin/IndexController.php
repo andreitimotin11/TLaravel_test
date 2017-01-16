a9b3ccde13918eb056da91a8203f7b05ea4bff32
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-16 20:38:47 +0200

    L13. Blade

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -9,34 +9,27 @@ use App\Http\Controllers\Controller;
 
 class IndexController extends Controller
 {
-    //
+
     public function show() {
-    	
-    //	view('welcome');
-    	
-    	//$data  = array('title'=>'Hello World','title2'=>'Hello world 3');
-		//return view('default.template',$data);//key=>value
-		
-		
-		//$view = view('default.template');
-		
-		//$view->with('title','Hello World 2');
-		//$view->with('title2','Hello World 3');
-		//$view->with('title3','Hello World 4');
+	    $array = array(
+
+		    'title'=>'Laravel Project',
+		    'data' =>[
+			    'one' => 'List 1',
+			    'two' => 'List 2',
+			    'three' => 'List 3',
+			    'four' => 'List 4',
+			    'five' => 'List 5'
+		    ],
+		    'dataI' =>['List 1','List 2','List 3','List 4','List 5'],
+
+		    'bvar' => true,
+		    'script' =>"<script>alert('hello')</script>"
+
+	    );
 		
 		if(view()->exists('default.index')) {
-			
-			//view()->name('default.template','myview');
-			//return view()->of('myview')->withTitle('Hello World');
-			
-			
-			//$view = view('default.template',['title'=>'Hello World'])->render();
-			//echo view('default.template',['title'=>'Hello World'])->getPath();
-			//echo $view;
-			///return;
-			
-			//$path = config('view.paths');
-			//return view()->file($path[0].'/default/layout.blade.php')->withTitle('Hello World');;
+
 			return view('default.index')->withTitle('Hello World');
 		}
 		
--- resources/views/default/about.blade.php
+++ resources/views/default/about.blade.php
@@ -1,1 +1,12 @@
 @extends('default.layouts.layout')
+
+@section('header')
+
+@endsection
+@section('sidebar')
+
+@endsection
+
+@section('content')
+
+@endsection
