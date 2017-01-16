2b2fba5869b6e4ea04e968d567df3f7f760befef
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-16 17:41:01 +0200

    L11

---
+++ app/Http/routes.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Routes File
+|--------------------------------------------------------------------------
+|
+| Here is where you will register all of the routes in an application.
+| It's a breeze. Simply tell Laravel the URIs it should respond to
+| and give it the controller to call when that URI is requested.
+|
+*/
+
+Route::get('/', ['as'=>'home','uses'=>'Admin\IndexController@show']);
+
+Route::get('/about',['uses'=>'Admin\AboutController@show','as'=>'about']);
+
+
+Route::get('/articles',['uses'=>'Admin\Core@getArticles','as'=>'articles']);
+
+
+Route::get('/article/{id}',[/*'middleware'=>'mymiddle:home',*/'uses'=>'Admin\Core@getArticle','as'=>'article'])/*->middleware(['mymiddle'])*/;
+
+
+/*
+|--------------------------------------------------------------------------
+| Application Routes
+|--------------------------------------------------------------------------
+|
+| This route group applies the "web" middleware group to every route
+| it contains. The "web" middleware group is defined in your HTTP
+| kernel and includes session state, CSRF protection, and more.
+|
+*/
+
+Route::group(['middleware' => ['web']], function () {
+    //
+    ////
+    
+    
+    ///
+    
+});
