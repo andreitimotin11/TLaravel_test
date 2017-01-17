ab5132b0b7f3ff4661a027c7efcbff367f575614
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-17 12:27:08 +0200

    L14. Class Request

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -21,7 +21,8 @@ Route::get('/articles',['uses'=>'Admin\Core@getArticles','as'=>'articles']);
 
 Route::get('/article/{id}',['uses'=>'Admin\Core@getArticle','as'=>'article']);
 
-
+Route::match(['get', 'post'], '/contacts', ['uses' =>'Admin\Core@getArticle',
+'as' => 'contacts']);
 /*
 |--------------------------------------------------------------------------
 | Application Routes
