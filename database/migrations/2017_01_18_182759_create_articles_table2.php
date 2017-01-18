1f6ccb2ca7a9b50461d74a4816f040e9e0c1c4ab
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-18 18:37:50 +0200

    L15. Migrations

---
+++ database/migrations/2017_01_18_182759_create_articles_table2.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArticlesTable2 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('articles', function (Blueprint $table) {
+
+
+            $table->increments('id');
+            $table->string('name', 100); // Varchar 100 symbols
+            $table->text('test'); // Varchar 100 symbols
+            $table->string('img', 255); // Varchar 100 symbols
+
+            $table->timestamps();
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('articles');
+    }
+}
