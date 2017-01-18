4bb5cdec1ca33cbec458bb7715f5257a1bc02467
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-18 19:44:20 +0200

    L16. Migrations

---
+++ database/seeds/ArticlesSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ArticlesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::insert('INSERT INTO `articles` (`name`, `text`, `img`) VALUES (?,?,?)',
+                   [
+	                   'Blog post',
+	                   '<p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>',
+                       'pic1.jpg'
+                   ]);
+    }
+}
