0aa055d8b0ba411c68a4260585d827ef9bbb0582
Andrei Timotin <andreitimotin11@gmail.com>
2017-01-22 16:53:09 +0200

    L18. SQL

--- app/Http/Controllers/Admin/AboutController.php
+++ app/Http/Controllers/Admin/AboutController.php
@@ -17,10 +17,13 @@ class AboutController extends Controller
 	public function show()
 	{
 		if (view()->exists('default.about')) {
+			//DB::insert("INSERT INTO `articles` (`name`, `text`) VALUES (?, ?)", ['test1', 'TEXT']);
 			$articles = DB::select('SELECT * FROM `articles` ');
-			dump($articles);
-			DB::insert("INSERT INTO `articles` (`name`, `text`) VALUES (?, ?)", ['test1', 'TEXT']);
+			//$result = DB::update('UPDATE `articles` SET `name` = ? WHERE `id`= ?', ['22222', 5]);
+			$result = DB::delete('DELETE FROM `articles` WHERE id = ?',[6]);
 
+			dump($result);
+			dump($articles);
 			return view('default.about')->withTitle('Hello World!');
 		}
 		abort(404);
